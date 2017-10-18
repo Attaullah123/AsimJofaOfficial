63225b57244dce66fbf09f8a096c0c8e5b24b35d
attaullah123 <attaa.779@gmail.com>
2017-10-18 15:26:23 +0500

    commit11.2

---
+++ app/src/main/java/com/cresset/asimjofaofficial/pushnotification/MyFirebaseMessagingService.java
@@ -0,0 +1,62 @@
+package com.cresset.asimjofaofficial.pushnotification;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+
+import com.cresset.asimjofaofficial.MainActivity;
+import com.cresset.asimjofaofficial.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //Displaying data in log
+        //It is optional
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+//        SharedPreferences preferences = getSharedPreferences(SHARED_PREF, MODE_PRIVATE);
+//        String checked = preferences.getString(SHARED_PREFForCheck,"");
+        //Calling method to generate notification
+        //if (checked == "NotSkip"){
+            sendNotification(remoteMessage.getNotification().getBody());
+        //}
+    }
+
+    //This method is only generating push notification
+    //It is same as we did in earlier posts
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Asim Jofa")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
